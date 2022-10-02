4ec84cf8adff252f6c42f7f1866e20ed761dca23
Lucas Messias <21.lucasmessias@gmail.com>
2022-10-01 22:59:23 -0300

    feat: add config

--- infrastructure/src/main/java/io/sanctuary/bs/api/infrastructure/Main.java
+++ infrastructure/src/main/java/io/sanctuary/bs/api/infrastructure/Main.java
@@ -3,6 +3,7 @@ package io.sanctuary.bs.api.infrastructure;
 import io.sanctuary.bs.api.infrastructure.configuration.WebServerConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.core.env.AbstractEnvironment;
 
 import javax.annotation.PostConstruct;
 import java.util.TimeZone;
@@ -11,7 +12,7 @@ import java.util.TimeZone;
 public class Main {
 
     public static void main(String[] args) {
-        //System.setProperty(AbstractEnvironment.DEFAULT_PROFILES_PROPERTY_NAME, "development");
+        System.setProperty(AbstractEnvironment.DEFAULT_PROFILES_PROPERTY_NAME, "development");
         SpringApplication.run(WebServerConfig.class, args);
     }
 
